d236ca86c1b9b5e194029ec709690e171c55cfba
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-01-09 19:48:22 +0100

    278. Creando la Vista de Testimoniales

--- server/routes/index.js
+++ server/routes/index.js
@@ -32,5 +32,11 @@ module.exports = function () {
             .catch(error => console.log(error))
     });
 
+    router.get('/testimoniales', (req, res) => {
+        res.render('testimoniales', {
+            pagina: 'Testimoniales'
+        });
+    });
+
     return router;
 }
