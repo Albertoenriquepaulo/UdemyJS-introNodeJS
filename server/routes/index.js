0a75cb807b038798a906a3221d89ae03fad5a9ed
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-01-09 19:30:54 +0100

    276. Creando la Vista para cada Viaje y Consultando la BD

--- server/routes/index.js
+++ server/routes/index.js
@@ -25,5 +25,12 @@ module.exports = function () {
             }))
             .catch((err) => console.log(error));
     });
+
+    router.get('/viajes/:id', (req, res) => {
+        Viaje.findByPk(req.params.id)
+            .then(viaje => res.render('oneviaje', { viaje }))
+            .catch(error => console.log(error))
+    });
+
     return router;
 }
