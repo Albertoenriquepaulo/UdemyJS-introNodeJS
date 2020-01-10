7d059030fcf70d0d9ee5beada3c7f5576f3ab6e9
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-01-10 14:27:42 +0100

    281. Llenando los Campos que el Usuario haya llenado correctamente

--- server/routes/index.js
+++ server/routes/index.js
@@ -45,6 +45,8 @@ module.exports = function () {
         // Y en el index.js el -> | const bodyParser = require('body-parser');|
         console.log(req.body);
         // Validar que todos los campos estén llenos
+        // Considerar que estos nombres son los que aparecen el html (testimoniales/index.pug)
+        // En la propiedad name de los inputs
         let { nombre, correo, mensaje } = req.body;
         let errores = [];
         if (!nombre) {
@@ -60,7 +62,16 @@ module.exports = function () {
         //Revisar por errores
         if (errores.length > 0) {
             // muestra la vista con errores
-
+            res.render('testimoniales', {
+                errores,
+                nombre,
+                correo,
+                mensaje
+                // Pasamos nombre, correo, mensaje para que en caso que ocurra error 
+                //no se pierda la info ya colocada por el usuario
+                // Y asi colocamos en en cada input del html en el campo value cada uno
+                //de estas variables
+            })
         } else {
             // almacenar en el BD
         }
