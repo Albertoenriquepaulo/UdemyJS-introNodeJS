98a7549256262a11c0a31623c148a0ba3d6d6a91
Alberto Paulo <Albertoenriquepaulo@yahoo.es>
2020-01-11 23:19:17 +0100

    295. Preparando el proyecto para deployment - variables de entorno (BD)

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+require('dotenv').config({ path: 'variables.env' });
 
 
 /**
@@ -6,8 +7,26 @@ const Sequelize = require('sequelize');
  * user: Albertoenriquepaulo@hotmail.com
  * pass:X--------T
  */
-// module.exports = new Sequelize('ZJkGXV74KD', 'ZJkGXV74KD', 'mPWZ9YhCMk', {
-//     host: 'remotemysql.com', ///pkg/phpmyadmin/
+module.exports = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
+    host: process.env.BD_HOST, ///pkg/phpmyadmin/
+    port: process.env.BD_PORT,
+    dialect: 'mysql',
+    define: {
+        timestamps: false
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    //operatorsAliases: false
+});
+
+
+// BD MySQL NAS, descomentamos la de arriba y tenemos la BD remotemysql.com
+// module.exports = new Sequelize('agenciadeviajes', 'alberto', '1234', {
+//     host: '192.168.1.130', ///pkg/phpmyadmin/
 //     port: '3306',
 //     dialect: 'mysql',
 //     define: {
@@ -21,21 +40,3 @@ const Sequelize = require('sequelize');
 //     },
 //     //operatorsAliases: false
 // });
-
-
-// BD MySQL NAS, descomentamos la de arriba y tenemos la BD remotemysql.com
-module.exports = new Sequelize('agenciadeviajes', 'alberto', '1234', {
-    host: '192.168.1.130', ///pkg/phpmyadmin/
-    port: '3306',
-    dialect: 'mysql',
-    define: {
-        timestamps: false
-    },
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-    //operatorsAliases: false
-});
